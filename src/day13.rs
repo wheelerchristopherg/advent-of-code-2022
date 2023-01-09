10460fb40cb78e1c45071578f628cc6c059f62a1
Christopher Wheeler <wheelerchristopherg@gmail.com>
2023-01-08 19:43:43 -0600

    non working day 13 part 2

--- src/day13.rs
+++ src/day13.rs
@@ -26,6 +26,64 @@ fn parse_line(line: &str) -> Vec<Item> {
     result
 }
 
+fn print_items(line: Vec<Item>) {
+    for item in line.iter() {
+        match item {
+            Item::Start => print!("["),
+            Item::End => print!("]"),
+            Item::Number(n) => print!(" {n} "),
+        }
+    }
+    println!();
+}
+
+fn compare(left: Vec<Item>, right: Vec<Item>) -> bool {
+    let mut l = left;
+    let mut r = right;
+    let mut right_order = true;
+    let mut i = 0;
+    loop {
+        let Some(li) = l.get(i) else {break};
+        let Some(ri) = r.get(i) else {break};
+        // println!("compare {li:?} to {ri:?}");
+
+        match (li, ri) {
+            (Item::Number(ln), Item::Number(rn)) => {
+                if rn < ln {
+                    right_order = false;
+                    break;
+                } else if ln < rn {
+                    right_order = true;
+                    break;
+                }
+            }
+            (Item::Start, Item::Start) => {
+                right_order = true;
+            }
+            (Item::End, Item::End) => {
+                right_order = true;
+            }
+            (Item::Start, Item::Number(_)) => {
+                r.insert(i, Item::Start);
+                r.insert(i + 2, Item::End);
+            }
+            (Item::Number(_), Item::Start) => {
+                l.insert(i, Item::Start);
+                l.insert(i + 2, Item::End);
+            }
+            (Item::End, _) => {
+                break;
+            }
+            (_, Item::End) => {
+                right_order = false;
+                break;
+            }
+        }
+        i += 1;
+    }
+    right_order
+}
+
 pub fn part1(input: &[String]) -> i32 {
     let mut pairs: Vec<(Vec<Item>, Vec<Item>)> = vec![];
     for lines in input.windows(2).step_by(3) {
@@ -38,50 +96,8 @@ pub fn part1(input: &[String]) -> i32 {
     let mut sum = 0;
     for (n, (left, right)) in pairs.into_iter().enumerate() {
         // println!("pair {}", n + 1);
-        let mut l = left;
-        let mut r = right;
-        let mut right_order = true;
-        let mut i = 0;
-        loop {
-            let Some(li) = l.get(i) else {break};
-            let Some(ri) = r.get(i) else {break};
-            // println!("compare {li:?} to {ri:?}");
 
-            match (li, ri) {
-                (Item::Number(ln), Item::Number(rn)) => {
-                    if rn < ln {
-                        right_order = false;
-                        break;
-                    } else if ln < rn {
-                        right_order = true;
-                        break;
-                    }
-                }
-                (Item::Start, Item::Start) => {
-                    right_order = true;
-                }
-                (Item::End, Item::End) => {
-                    right_order = true;
-                }
-                (Item::Start, Item::Number(_)) => {
-                    r.insert(i, Item::Start);
-                    r.insert(i + 2, Item::End);
-                }
-                (Item::Number(_), Item::Start) => {
-                    l.insert(i, Item::Start);
-                    l.insert(i + 2, Item::End);
-                }
-                (Item::End, _) => {
-                    break;
-                }
-                (_, Item::End) => {
-                    right_order = false;
-                    break;
-                }
-            }
-            i += 1;
-        }
-
+        let right_order = compare(left, right);
         if right_order {
             // println!("order is correct");
             sum += (n as i32) + 1;
@@ -92,7 +108,46 @@ pub fn part1(input: &[String]) -> i32 {
 }
 
 pub fn part2(input: &[String]) -> i32 {
-    todo!()
+    let mut packets: Vec<Vec<Item>> = vec![];
+    for lines in input.windows(2).step_by(3) {
+        // println!("lines: {lines:?}");
+        packets.push(parse_line(&lines[0]));
+        packets.push(parse_line(&lines[1]));
+    }
+
+    let mut sorted: Vec<Vec<Item>> = vec![];
+
+    packets.push(vec![
+        Item::Start,
+        Item::Start,
+        Item::Number(6),
+        Item::End,
+        Item::End,
+    ]);
+    packets.push(vec![
+        Item::Start,
+        Item::Start,
+        Item::Number(2),
+        Item::End,
+        Item::End,
+    ]);
+
+    for packet in packets.into_iter().skip(5).take(2) {
+        let mut i = sorted.len();
+        for ind in 0..sorted.len() {
+            println!("comparing:\n\t{packet:?}\n\t{:?}", sorted[ind]);
+            if compare(packet.clone(), sorted[ind].clone()) {
+                i = ind;
+            }
+        }
+        println!("inserted {packet:?} at {i}\n");
+        sorted.insert(i, packet.clone());
+    }
+
+    for line in sorted {
+        print_items(line);
+    }
+    0
 }
 
 #[cfg(test)]
